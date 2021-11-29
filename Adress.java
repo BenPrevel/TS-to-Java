525325f05459742b5f49e155021465c52cac3597
BenPrevel <403rutabaga@gmail.com>
2021-11-29 19:52:33 +0100

    first commit

---
+++ Adress.java
@@ -0,0 +1,8 @@
+public class Adress {
+    
+    private String number ;
+    private String street;
+    private String city;
+    private String country;
+    
+}
---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class User {
+    
+    private Date birthdate;
+    private int size;
+    private Photo photo[];
+    private Adress address; 
+}
