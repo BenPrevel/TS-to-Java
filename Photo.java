3d0561204847d11e5b41a7377e435ae40833f297
BenPrevel <403rutabaga@gmail.com>
2021-11-30 09:45:45 +0100

    exercice corrected

--- Photo.java
+++ Photo.java
@@ -4,4 +4,16 @@ public class Photo {
 	private String filename;
 	private int width;
 	private int height;
+
+	public String getUrl() {return this.url;}
+    public void setUrl(String url) {this.url = url;};
+
+	public String getfilname() {return this.filename;}
+    public void setfilename(String filename) {this.filename = filename;};
+
+	public int getwidth() {return this.width;}
+    public void setwidth(int width) {this.width = width;};
+
+	public int getHeight() {return this.height;}
+    public void setHeight(int height) {this.height = height;};
 }
--- User.java
+++ User.java
@@ -4,6 +4,18 @@ public class User {
     
     private Date birthdate;
     private int size;
-    private Photo photo[];
-    private Adress address; 
+    private Photo photo;
+    private Address address;
+
+    public Date getBirthdate() {return this.birthdate;}
+    public void setBirthdate(Date birthdate) {this.birthdate = birthdate;};
+
+    public int getSize() {return this.size;}
+    public void setSize(int size) {this.size = size;};
+
+    public Photo getPhoto() {return this.photo;}
+    public void setPhoto(Photo photo) {this.photo = photo;};
+
+    public Address getAdress() {return this.address;}
+    public void setAdress(Address address) {this.address = address;};
 }
